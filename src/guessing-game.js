7db97a6ce4c88945ef07a2d8617e5e211bf75a10
me4 <abduhamid.sultonov@gmail.com>
2017-09-25 14:17:16 +0300

    implemented

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
